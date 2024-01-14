0c04720eec38fab65b99715b14d450309700b96a
Oye Oloyede <oyekunlemac@gmail.com>
2024-01-14 19:08:03 +0000

    Added the trace system call

--- kernel/proc.c
+++ kernel/proc.c
@@ -302,6 +302,9 @@ fork(void)
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
+  // copy the trace mask from parent to child.
+  np->trace_mask = p->trace_mask;
+
   // increment reference counts on open file descriptors.
   for(i = 0; i < NOFILE; i++)
     if(p->ofile[i])
