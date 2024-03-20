7bab6e3fa46c2cc319610d6fb274855d58ac7766
Oye Oloyede <oyekunlemac@gmail.com>
2024-03-20 10:28:41 +0000

    cow solution

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,7 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+void 			safe_increase_rc(void* pa);
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -147,6 +148,7 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
+int 			page_fault_handler(pagetable_t, uint64);
 
 // uart.c
 void            uartinit(void);
--- kernel/vm.c
+++ kernel/vm.c
@@ -5,6 +5,8 @@
 #include "riscv.h"
 #include "defs.h"
 #include "fs.h"
+#include "spinlock.h"
+#include "proc.h"
 
 /*
  * the kernel's page table.
@@ -313,30 +315,37 @@ int
 uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
 {
   pte_t *pte;
-  uint64 pa, i;
-  uint flags;
-  char *mem;
+  uint64 i;
+  //uint flags;
+  //char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
       panic("uvmcopy: pte should exist");
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
-    pa = PTE2PA(*pte);
-    flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
-      goto err;
-    }
+    //pa = PTE2PA(*pte);
+    //flags = PTE_FLAGS(*pte);
+    //if((mem = kalloc()) == 0)
+      //goto err;
+    //memmove(mem, (char*)pa, PGSIZE);
+    //if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
+      //kfree(mem);
+      //goto err;
+    //}
+    *pte |= PTE_C;
+    *pte &= ~PTE_W;
+
+    pte_t *n_pte = walk(new, i, 1);
+    *n_pte = *pte;
+
+    safe_increase_rc((void*)PTE2PA(*pte));
   }
   return 0;
 
- err:
-  uvmunmap(new, 0, i / PGSIZE, 1);
-  return -1;
+ //err:
+  //uvmunmap(new, 0, i / PGSIZE, 1);
+  //return -1;
 }
 
 // mark a PTE invalid for user access.
@@ -365,6 +374,10 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
     va0 = PGROUNDDOWN(dstva);
     if(va0 >= MAXVA)
       return -1;
+
+    if (page_fault_handler(pagetable, va0) != 0)
+      return -1;
+
     pte = walk(pagetable, va0, 0);
     if(pte == 0 || (*pte & PTE_V) == 0 || (*pte & PTE_U) == 0 ||
        (*pte & PTE_W) == 0)
